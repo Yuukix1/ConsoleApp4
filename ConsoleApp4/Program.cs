f7d601f41ca797acc27b2c3206a92fb9a958f55c
BabakAdam <babaka@kkszki.hu>
2025-09-24 08:26:37 +0200

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Butorgyar
+{
+    public class Butorlap
+    {
+
+        public const int TartolapArNm = 5000;
+        public const int HatlapArNm = 500;
+
+        public double HosszM { get; }
+        public double SzelessegM { get; }
+        public string Tipus { get; }
+
+
+        public Butorlap(string tipus, double hosszCm, double szelessegCm)
+        {
+            Tipus = tipus.ToLower();
+            HosszM = hosszCm / 100.0;
+            SzelessegM = szelessegCm / 100.0;
+        }
+
+        public int ArSzamol()
+        {
+            double terulet = HosszM * SzelessegM;
+            int egysegar = (Tipus == "tartolap") ? TartolapArNm : HatlapArNm;
+            return (int)Math.Round(terulet * egysegar);
+        }
+    }
+}
