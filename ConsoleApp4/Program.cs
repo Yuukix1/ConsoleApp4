bec195e2e4258497e22f6ab1d10754399e81e99b
BabakAdam <babaka@kkszki.hu>
2025-09-24 08:27:15 +0200

    Befejezés

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -4,15 +4,15 @@ namespace Butorgyar
 {
     public class Butorlap
     {
-
+        
         public const int TartolapArNm = 5000;
         public const int HatlapArNm = 500;
 
         public double HosszM { get; }
         public double SzelessegM { get; }
-        public string Tipus { get; }
+        public string Tipus { get; } 
 
-
+        
         public Butorlap(string tipus, double hosszCm, double szelessegCm)
         {
             Tipus = tipus.ToLower();
@@ -22,9 +22,41 @@ namespace Butorgyar
 
         public int ArSzamol()
         {
-            double terulet = HosszM * SzelessegM;
+            double terulet = HosszM * SzelessegM; 
             int egysegar = (Tipus == "tartolap") ? TartolapArNm : HatlapArNm;
             return (int)Math.Round(terulet * egysegar);
+        }
+    }
+
+    public class Butor
+    {
+        private readonly Butorlap[] lapok;
+        private int lapokSzama;
+
+        public Butor(int kapacitas = 100)
+        {
+            lapok = new Butorlap[kapacitas];
+            lapokSzama = 0;
+        }
+
+        public bool LapHozzaad(Butorlap lap)
+        {
+            if (lapokSzama >= lapok.Length) return false;
+            lapok[lapokSzama++] = lap;
+            return true;
+        }
+
+        public int ArSzamol()
+        {
+            int osszes = 0;
+            for (int i = 0; i < lapokSzama; i++)
+                osszes += lapok[i].ArSzamol();
+            return osszes;
+        }
+
+        public override string ToString()
+        {
+            return $"Butor - lapok: {lapokSzama}, ar {ArSzamol()} Ft";
         }
     }
 }
